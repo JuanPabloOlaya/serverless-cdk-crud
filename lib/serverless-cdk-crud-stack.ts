c13019d24ec2f3cc97755630838714843405b7de
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2022-05-08 22:46:55 -0500

    Creating the basic stack to start the project building

--- lib/serverless-cdk-crud-stack.ts
+++ lib/serverless-cdk-crud-stack.ts
@@ -1,8 +1,58 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+  Stack,
+  StackProps,
+  aws_lambda_nodejs as Lambda,
+  aws_apigateway as APIGateway,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class ServerlessCdkCrudStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const createUserLambda: Lambda.NodejsFunction = new Lambda.NodejsFunction(this, "createUser", {
+      entry: "src/apps/admin/lambdas/index.ts",
+      handler: "createUser",
+      bundling: {
+        preCompilation: true,
+      },
+    });
+
+    const getUserLambda: Lambda.NodejsFunction = new Lambda.NodejsFunction(this, "getUser", {
+      entry: "src/apps/admin/lambdas/index.ts",
+      handler: "getUser",
+      bundling: {
+        preCompilation: true,
+      },
+    });
+
+    const updateUserLambda: Lambda.NodejsFunction = new Lambda.NodejsFunction(this, "updateUser", {
+      entry: "src/apps/admin/lambdas/index.ts",
+      handler: "updateUser",
+      bundling: {
+        preCompilation: true,
+      },
+    });
+
+    const deleteUserLambda: Lambda.NodejsFunction = new Lambda.NodejsFunction(this, "deleteUser", {
+      entry: "src/apps/admin/lambdas/index.ts",
+      handler: "deleteUser",
+      bundling: {
+        preCompilation: true,
+      },
+    });
+
+    const apiGateway: APIGateway.LambdaRestApi = new APIGateway.LambdaRestApi(this, "crudGateway", {
+      handler: createUserLambda,
+      proxy: false,
+    });
+
+    const userResource: APIGateway.Resource = apiGateway.root.addResource("user");
+    userResource.addMethod("POST", new APIGateway.LambdaIntegration(createUserLambda));
+
+    const userParamsResource: APIGateway.Resource = userResource.addResource("{id}");
+    userParamsResource.addMethod("GET", new APIGateway.LambdaIntegration(getUserLambda));
+    userParamsResource.addMethod("PUT", new APIGateway.LambdaIntegration(updateUserLambda));
+    userParamsResource.addMethod("DELETE", new APIGateway.LambdaIntegration(deleteUserLambda));
   }
 }
