088da8393aeb3abb12aa34b6d6b7b17e92bc72e8
Juan Pablo Olaya <juanpa-1920@hotmail.com>
2022-05-08 21:48:20 -0500

    Cleaning project

--- test/serverless-cdk-crud.test.ts
+++ test/serverless-cdk-crud.test.ts
@@ -1,17 +1,1 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as ServerlessCdkCrud from '../lib/serverless-cdk-crud-stack';
-
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/serverless-cdk-crud-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new ServerlessCdkCrud.ServerlessCdkCrudStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+test('SQS Queue Created', () => {});
